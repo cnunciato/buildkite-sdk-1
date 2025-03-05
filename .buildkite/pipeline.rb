576b0ece97a4b941e35b6c1e402ca97200de4a09
Zack Chase <zachary@buildkite.com>
2025-03-04 16:01:03 -0800

    Force the publish pipeline to run

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -18,20 +18,33 @@ plugins = [
 ]
 
 # If the job has an associated tag that looks like a new version, add a publish step.
-if !tag.nil? && tag.start_with?("v")
-  commands.push("npm run publish")
-  plugins.push({ "rubygems-oidc#v0.2.0": { role: "rg_oidc_akr_emf87k6zphtb7x7adyrk" } })
-  plugins.push({ "aws-assume-role-with-web-identity#v1.0.0": {
-    "role-arn": "arn:aws:iam::597088016345:role/marketing-website-production-pipeline-role"
-  }})
-  plugins.push({ "aws-ssm#v1.0.0": {
-    parameters: {
-      NPM_TOKEN: "prod/buildkite-sdk/npm-token",
-      PYPI_TOKEN: "prod/buildkite-sdk/pypi-token",
-      GITHUB_TOKEN: "prod/buildkite-sdk/github-token"
-    }
-  }})
-end
+# if !tag.nil? && tag.start_with?("v")
+#   commands.push("npm run publish")
+#   plugins.push({ "rubygems-oidc#v0.2.0": { role: "rg_oidc_akr_emf87k6zphtb7x7adyrk" } })
+#   plugins.push({ "aws-assume-role-with-web-identity#v1.0.0": {
+#     "role-arn": "arn:aws:iam::597088016345:role/marketing-website-production-pipeline-role"
+#   }})
+#   plugins.push({ "aws-ssm#v1.0.0": {
+#     parameters: {
+#       NPM_TOKEN: "prod/buildkite-sdk/npm-token",
+#       PYPI_TOKEN: "prod/buildkite-sdk/pypi-token",
+#       GITHUB_TOKEN: "prod/buildkite-sdk/github-token"
+#     }
+#   }})
+# end
+
+commands.push("npm run publish")
+plugins.push({ "rubygems-oidc#v0.2.0": { role: "rg_oidc_akr_emf87k6zphtb7x7adyrk" } })
+plugins.push({ "aws-assume-role-with-web-identity#v1.0.0": {
+  "role-arn": "arn:aws:iam::597088016345:role/marketing-website-production-pipeline-role"
+}})
+plugins.push({ "aws-ssm#v1.0.0": {
+  parameters: {
+    NPM_TOKEN: "prod/buildkite-sdk/npm-token",
+    PYPI_TOKEN: "prod/buildkite-sdk/pypi-token",
+    GITHUB_TOKEN: "prod/buildkite-sdk/github-token"
+  }
+}})
 
 pipeline.add_step(
   label: ":hammer_and_wrench: Install, test, build, publish",
